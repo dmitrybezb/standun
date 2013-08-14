4ecaaa2b7d69360b061a0577d9e5d12ba549842f
dmitrybezb <dmitrybezb@gmail.com>
2013-08-14 11:34:31 +0000

    123

--- web.js
+++ web.js
@@ -3,15 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World! =)');
 });
 
 
-
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
