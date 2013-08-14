83976a7c28ba86da1e032cc4174d473ed8e43c0c
dmitrybezb <dmitrybezb@gmail.com>
2013-08-14 11:21:36 +0000

    modify web.js

--- web.js
+++ web.js
@@ -6,6 +6,13 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
